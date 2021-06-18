0c9740622bfa01fc8c3dc1eaa8cf8d36c892c348
MiqueiasLima <miqueiaslima@outlook.com>
2021-06-18 10:28:15 -0300

    Construindo Layout do App

---
+++ lib/Home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("imagens/youtube.png",
+        width: 98),
+        iconTheme: IconThemeData(
+          color: Colors.grey,
+          opacity: 1
+        ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.videocam)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.account_circle)),
+        ],
+      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon:Icon(Icons.home),
+          label: 'teste'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: 'teste'),
+          BottomNavigationBarItem(icon: Icon(Icons.http),label: 'teste'),
+        ],
+      ),
+    );
+  }
+}
